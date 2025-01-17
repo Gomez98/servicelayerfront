2d62d771d570290d22e71e4f96db4880b1196cc9
German Gomez <proyecto4@grupollamagas.com>
2025-01-17 16:19:57 -0500

    add config4

--- server.js
+++ server.js
@@ -1,21 +1,14 @@
-const https = require('https');
-const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
-// Configurar rutas (modifica según tu app)
+// Configurar rutas
 app.get('/', (req, res) => {
-  res.send('¡Tu frontend está funcionando en HTTPS!');
+  res.send('¡Tu frontend está funcionando detrás de Apache!');
 });
 
-// Cargar certificados SSL
-const options = {
-  key: fs.readFileSync('/etc/letsencrypt/live/sv-hol0uzz7c3.cloud.elastika.pe/privkey.pem'),
-  cert: fs.readFileSync('/etc/letsencrypt/live/sv-hol0uzz7c3.cloud.elastika.pe/fullchain.pem')
-};
-
-// Iniciar servidor HTTPS en el puerto 443
-https.createServer(options, app).listen(443, () => {
-  console.log('Servidor HTTPS corriendo en el puerto 443');
+// Iniciar servidor HTTP en el puerto 8080
+const PORT = 8080;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
