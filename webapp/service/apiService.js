1b5ecdcfaa4e1169c913f1f78b70161ec5897c0f
German Gomez <proyecto4@grupollamagas.com>
2025-01-17 17:20:41 -0500

    config new

--- webapp/service/apiService.js
+++ webapp/service/apiService.js
@@ -2,7 +2,7 @@ sap.ui.define([], function () {
     "use strict";
 
     const apiClient = axios.create({
-        baseURL: "http://sv-hol0uzz7c3.cloud.elastika.pe:8081",
+        baseURL: "https://sv-hol0uzz7c3.cloud.elastika.pe:8081",
         headers: {
             "Content-Type": "application/json"
         }
