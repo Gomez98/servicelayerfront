c0e41348f686d41fdc4a0d205392405e9806749e
German Gomez <proyecto4@grupollamagas.com>
2025-01-20 11:34:45 -0500

    add confgi 6

--- server.js
+++ server.js
@@ -3,26 +3,20 @@ const path = require("path");
 
 const app = express();
 
-// Puerto donde se ejecutará el servidor
 const PORT = 8080;
 
-// Ruta para servir los archivos estáticos
 const staticPath = path.join(__dirname, "webapp");
 
-// Middleware para servir los archivos SAPUI5
 app.use(express.static(staticPath));
 
-// Ruta por defecto para cualquier archivo no encontrado
 app.get("*", (req, res) => {
     res.sendFile(path.join(staticPath, "index.html"));
 });
 
-// Iniciar el servidor
 app.listen(PORT, "0.0.0.0", () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
 
-// Manejo de errores en caso de que algo falle
 process.on("uncaughtException", (err) => {
     console.error("Error no controlado:", err);
     process.exit(1); // Salida del proceso en caso de error crítico
